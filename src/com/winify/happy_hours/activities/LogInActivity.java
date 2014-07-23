59809894635c0b2a2d99a0c6f187504d9b5e1d69
vasilenastas <vasile110593@yahoo.com>
2014-07-23 11:29:29 +0300

    synchronize with server 2nd try

--- src/com/winify/happy_hours/activities/LogInActivity.java
+++ src/com/winify/happy_hours/activities/LogInActivity.java
@@ -1,6 +1,8 @@
 package com.winify.happy_hours.activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,14 +18,15 @@ import com.winify.happy_hours.controller.ServiceGateway;
 import com.winify.happy_hours.controller.TrackerController;
 import com.winify.happy_hours.listeners.ServiceListener;
 import com.winify.happy_hours.models.User;
-import com.winify.happy_hours.service.ServiceSettings;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
+
+
 
 public class LogInActivity extends Activity implements ServiceListener {
     private EditText login;
     private EditText password;
-    private ApplicationPreferencesActivity preferences;
+    private ApplicationPreferences preferences;
     private TrackerController trackerController;
 
     @Override
@@ -34,7 +37,7 @@ public class LogInActivity extends Activity implements ServiceListener {
         ServiceGateway serviceGateway = new ServiceGateway(LogInActivity.this);
         trackerController = serviceGateway.getTrackerController(this);
 
-        preferences = new ApplicationPreferencesActivity(this);
+        preferences = new ApplicationPreferences(this);
 
         login = (EditText) findViewById(R.id.login);
         password = (EditText) findViewById(R.id.password);
@@ -43,8 +46,23 @@ public class LogInActivity extends Activity implements ServiceListener {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (login.getText().toString().length() > 0 && password.getText().toString().length() > 0) {
-                    getKeyToken(login.getText().toString(), password.getText().toString());
+                if (preferences.getIp().equals("") || preferences.getPort().equals("")) {
+
+
+                    AlertDialog ad = new AlertDialog.Builder(LogInActivity.this).create();
+                    ad.setCancelable(false); // This blocks the 'BACK' button
+                    ad.setMessage("Check youre Ip Address and Port in  settings");
+                    ad.setButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    ad.show();
+                } else {
+                    if (login.getText().toString().length() > 0 && password.getText().toString().length() > 0) {
+                        getKeyToken(login.getText().toString(), password.getText().toString());
+                    }
                 }
             }
         });
@@ -62,23 +80,31 @@ public class LogInActivity extends Activity implements ServiceListener {
 
     @Override
     public void onServerFail(RetrofitError error) {
+        AlertDialog ad = new AlertDialog.Builder(this).create();
+        ad.setCancelable(false); // This blocks the 'BACK' button
+        ad.setMessage("Please check youre Username and Password or youre connection with Server");
+        ad.setButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        ad.show();
     }
 
     @Override
     public void onUsersList(User user) {
         preferences.savePreferences(Extra.KEY_TOKEN, user.getToken());
-        Toast.makeText(LogInActivity.this,  preferences.getKeyToken(), Toast.LENGTH_LONG).show();
+        Toast.makeText(LogInActivity.this, preferences.getKeyToken(), Toast.LENGTH_LONG).show();
         redirectHomePage();
         finish();
     }
 
     public void getKeyToken(String login, String password) {
-        User user = new User(login, password,"","","","","");
+        User user = new User(login, password, "", "", "", "", "");
         preferences.removePreferences(Extra.KEY_TOKEN);
         trackerController.geToken(user);
-
     }
-
 
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater Inflater = getMenuInflater();
@@ -90,16 +116,12 @@ public class LogInActivity extends Activity implements ServiceListener {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.startSettings: {
-
-                Intent intent = new Intent(LogInActivity.this, ServiceSettings.class);
+                Intent intent = new Intent(LogInActivity.this, ServiceSettingsActivity.class);
                 startActivity(intent);
-
             }
             break;
         }
         return super.onOptionsItemSelected(item);
     }
-
-
 }
 
